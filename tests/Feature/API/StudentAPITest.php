93f95dee8064b45afb12126938c85e8b8acc1dfc
Phat Panhareach <111555521+Re4ch-Jay@users.noreply.github.com>
2023-09-28 21:46:03 +0700

    refactor test

--- tests/Feature/API/StudentAPITest.php
+++ tests/Feature/API/StudentAPITest.php
@@ -12,15 +12,30 @@ use Tests\TestCase;
 class StudentAPITest extends TestCase
 {
     use RefreshDatabase;
+
+    private $admin;
+    private $major;
+    private $student;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->admin = User::factory()->create();
+        $this->major = Major::factory()->create();
+        $this->student = Student::factory()->create([
+            'major_id' => $this->major->id,
+        ]);
+    }
+
     /**
      * A basic feature test example.
      */
     public function test_admin_can_get_all_students(): void
     {
 
-        $admin = User::factory()->create();
         
-        $response = $this->actingAs($admin)->get('/api/v1/students');
+        $response = $this->actingAs($this->admin)->get('/api/v1/students');
 
         $response->assertStatus(200);
 
@@ -29,13 +44,7 @@ class StudentAPITest extends TestCase
     public function test_admin_can_update_student(): void
     {
 
-        $admin = User::factory()->create();
-        $major = Major::factory()->create();
-        $student = Student::factory()->create([
-            'major_id' => $major->id,
-        ]);
-        
-        $response = $this->actingAs($admin)->put('/api/v1/students/'. $student->id, [
+        $response = $this->actingAs($this->admin)->put('/api/v1/students/'. $this->student->id, [
             'first_name' => 'John',
             'last_name' => 'Doe',
             'dob' => '2015-12-17',
@@ -44,7 +53,7 @@ class StudentAPITest extends TestCase
             'phone_number' => '012345678',
             'gender' => 'Male',
             'email' => 'john@john.com',
-            'major_id' => $major->id,
+            'major_id' => $this->major->id,
         ]);
 
         $response->assertStatus(200);
